662368370ba5caa6746fcfdb849efdba2848552d
Dennis Porto <60332417+dennisreis-dev@users.noreply.github.com>
2022-12-28 15:58:08 -0300

    Aulas 2 e 3

--- Projetos/aula1/aula1.py
+++ Projetos/aula1/aula1.py
@@ -1,1 +1,15 @@
-print("CNP Holding Brasil")
+"""
+DocString
+E escrever o que eu
+quiser
+
+Exercícios do curso - aula 1
+
+"""
+
+''' Usar para escrever suas notas '''
+
+# Permite escrever um comentário
+print(123)  # Na frente
+# Abaixo
+print(456)
