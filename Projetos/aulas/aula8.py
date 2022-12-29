30cddce5a7d3829ee43f181ae88576c28a6f8b2d
Dennis Porto <60332417+dennisreis-dev@users.noreply.github.com>
2022-12-29 11:47:40 -0300

    Subindo as aulas 7, 8, 9 e 10

---
+++ Projetos/aulas/aula8.py
@@ -0,0 +1,12 @@
+nome = 'Jão'
+sobrenome = 'da Silva'
+idade = 49
+ano_nascimento = 2022 - idade
+maior_idade = idade >= 18
+altura_metros = 1.79
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('É maior de idade?', maior_idade)
+print('Altura em metros', altura_metros)
