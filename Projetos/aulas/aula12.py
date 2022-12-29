86cb46bf4e85fce4c55796955830abcc8bfab089
Dennis Porto <60332417+dennisreis-dev@users.noreply.github.com>
2022-12-29 16:09:34 -0300

    Subindo as atualizações das aulas 9 e 10 e subindo as aulas de 11 até a aula 20

---
+++ Projetos/aulas/aula12.py
@@ -0,0 +1,12 @@
+nome = 'Luiz Otávio'
+altura = 1.79
+peso = 92
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
+# Luiz Otávio tem 1.79 de altura,
+# pesa 92 quilos e seu IMC é
+# 28.71321119815237
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Curso-de-Python-3
-## Curso de Python 3 do Básico Ao Avançado (com projetos reais)
-### Exercícios do curso
+## Do Básico Ao Avançado (com projetos reais)
+### Você encontrará nesse repositorio os exercícios do curso
